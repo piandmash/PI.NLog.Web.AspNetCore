1f29b1e62c3e3b0d7f6281267cdb81160d4cc2eb
Pete Cleary <pete@piandmash.com>
2018-09-20 06:58:29 +0100

    init

---
+++ PI.NLog.Web.AspNetCore/Extensions/RequestIpLayoutRenderer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using NLog;
+using NLog.LayoutRenderers;
+using NLog.Web.LayoutRenderers;
+using System.Text;
+
+// With thanks to Ian Williams from Takka Productions for this Extension from his TurboDash
+
+namespace PI.Core.NLog.Extensions
+{
+    /// <summary>
+    /// Render the request IP for ASP.NET Core
+    /// </summary>
+    /// <example>
+    /// <code lang="NLog Layout Renderer">
+    /// ${aspnet-request-ip}
+    /// </code>
+    /// </example>
+    [LayoutRenderer("aspnet-request-ip")]
+    public class RequestIpLayoutRenderer : AspNetLayoutRendererBase
+    {
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var httpContext = HttpContextAccessor.HttpContext;
+            if (httpContext == null)
+            {
+                return;
+            }
+            builder.Append(httpContext.Connection.RemoteIpAddress);
+        }
+    }
+}
---
+++ PI.NLog.Web.AspNetCore/Extensions/ServerIpLayoutRenderer.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+using NLog.LayoutRenderers;
+using NLog.Web.LayoutRenderers;
+using System.Text;
+
+// With thanks to Ian Williams from Takka Productions for this Extension from his TurboDash
+
+namespace PI.Core.NLog.Extensions
+{
+    /// <summary>
+    /// Render the request IP for ASP.NET Core
+    /// </summary>
+    /// <example>
+    /// <code lang="NLog Layout Renderer">
+    /// ${aspnet-request-ip}
+    /// </code>
+    /// </example>
+    [LayoutRenderer("aspnet-server-ip")]
+    public class ServerIpLayoutRenderer : AspNetLayoutRendererBase
+    {
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var httpContext = HttpContextAccessor.HttpContext;
+            if (httpContext == null)
+            {
+                return;
+            }
+            builder.Append(httpContext.Connection.LocalIpAddress);
+        }
+    }
+}
